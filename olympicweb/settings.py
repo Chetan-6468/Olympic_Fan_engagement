8cf4054f81b82576fa9cc146691e0f7c6327c020
sanya <sanya@mirja.in>
2023-07-31 15:00:18 +0530

    finallast

--- olympicweb/settings.py
+++ olympicweb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-tk=s5*x0a-94h!i2ew*(du*y@=u93m8y)$=t(4-b1&elmhbf!y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Security settings
